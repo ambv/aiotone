413593957035cbfab1a36aefc23126d393833852
Łukasz Langa <lukasz@langa.pl>
2020-04-17 00:13:04 +0200

    New Step 1

--- aiotone/__main__.py
+++ aiotone/__main__.py
@@ -4,22 +4,11 @@ from typing import *
 import asyncio
 import time
 
-from attr import dataclass
+import attr
 import click
 import uvloop
 
-from .midi import (
-    MidiOut,
-    NOTE_OFF,
-    NOTE_ON,
-    CLOCK,
-    START,
-    STOP,
-    CONTROL_CHANGE,
-    ALL_NOTES_OFF,
-    get_ports,
-)
-
+from .midi import get_ports, silence, MidiOut, CLOCK, START, STOP, NOTE_ON, NOTE_OFF
 
 # types
 EventDelta = float  # in seconds
@@ -28,46 +17,25 @@ MidiPacket = List[int]
 MidiMessage = Tuple[MidiPacket, EventDelta, TimeStamp]
 
 
-@dataclass
+@attr.dataclass
 class Performance:
     drums: MidiOut
     bass: MidiOut
     pulse_delta: float = 0.02  # 125 BPM (0.02 / 60 / 24 pulses per quarter note)
 
     async def play_drum(
-        self, note: int, pulses: int, volume: int = 127, decay: float = 0.5
-    ) -> None:
-        await self.play(self.drums, 9, note, pulses, volume, decay)
-
-    async def play_bass(
-        self, note: int, pulses: int, volume: int = 127, decay: float = 0.5
-    ) -> None:
-        await self.play(self.bass, 0, note, pulses, volume, decay)
-
-    async def play(
-        self,
-        out: MidiOut,
-        channel: int,
-        note: int,
-        pulses: int,
-        volume: int,
-        decay: float = 0.5,
+        self, note: int, pulses: int, volume: int = 127, decay: float = 0.5,
     ) -> None:
         note_on_length = int(round(pulses * decay, 0))
         rest_length = pulses - note_on_length
-        out.send_message([NOTE_ON | channel, note, volume])
+        channel = 9
+        self.drums.send_message([NOTE_ON | channel, note, volume])
         await self.wait(note_on_length)
-        out.send_message([NOTE_OFF | channel, note, volume])
+        self.drums.send_message([NOTE_OFF | channel, note, volume])
         await self.wait(rest_length)
 
     async def wait(self, pulses: int) -> None:
         await asyncio.sleep(pulses * self.pulse_delta)
-
-
-@click.command()
-def main() -> None:
-    uvloop.install()
-    asyncio.run(async_main())
 
 
 async def async_main() -> None:
@@ -78,10 +46,10 @@ async def async_main() -> None:
         from_circuit, to_circuit = get_ports("Circuit", clock_source=True)
         from_mono_station, to_mono_station = get_ports("Circuit Mono Station")
     except ValueError as port:
-        click.secho(f"{port} not connected", fg="red", err=True)
+        click.secho(f"{port} is not available", fg="red", err=True)
         raise click.Abort
 
-    def midi_callback(msg, data=None):
+    def midi_callback(msg: List[int], data: Any = None) -> None:
         sent_time = time.time()
         midi_message, event_delta = msg
         try:
@@ -89,19 +57,17 @@ async def async_main() -> None:
                 queue.put_nowait, (midi_message, event_delta, sent_time)
             )
         except BaseException as be:
-            click.secho(f"callback exc: {type(be)} {be}", fg="red", err=True)
+            click.secho(f"callback exc: {be}", fg="red", err=True)
 
     from_circuit.set_callback(midi_callback)
-    from_mono_station.close_port()  # we won't be using that one now
+    from_mono_station.close_port()
     performance = Performance(drums=to_circuit, bass=to_mono_station)
     try:
         await midi_consumer(queue, performance)
     except asyncio.CancelledError:
         from_circuit.cancel_callback()
-        to_circuit.send_message([STOP])
-        to_mono_station.send_message([STOP])
-        to_mono_station.send_message([CONTROL_CHANGE | 0, ALL_NOTES_OFF, 0])
-        to_mono_station.send_message([CONTROL_CHANGE | 1, ALL_NOTES_OFF, 0])
+        silence(to_circuit)
+        silence(to_mono_station)
 
 
 async def midi_consumer(
@@ -113,7 +79,7 @@ async def midi_consumer(
         msg, delta, sent_time = await queue.get()
         latency = time.time() - sent_time
         if __debug__:
-            print(f"{msg}\tevent delta: {delta:.4f}\tlatency: {latency:.4f}")
+            click.echo(f"{msg}\tevent delta: {delta:.4f}\tlatency: {latency:.4f}")
         if msg[0] == CLOCK:
             performance.bass.send_message(msg)
             if last_msg[0] == CLOCK:
@@ -127,7 +93,7 @@ async def midi_consumer(
             if drums is not None:
                 drums.cancel()
                 drums = None
-                performance.drums.send_message([CONTROL_CHANGE | 9, ALL_NOTES_OFF, 0])
+                silence(performance.drums)
         last_msg = msg
 
 
@@ -139,6 +105,12 @@ async def drum_machine(performance: Performance) -> None:
 
     while True:
         await performance.play_drum(b_drum, 24)
+
+
+@click.command()
+def main() -> None:
+    uvloop.install()
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
