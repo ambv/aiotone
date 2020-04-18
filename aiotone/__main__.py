9154d3960d907adb0b81d353483204f7c696ffdf
Łukasz Langa <lukasz@langa.pl>
2020-04-18 13:42:54 +0200

    MIDI callback, signature

--- aiotone/__main__.py
+++ aiotone/__main__.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 from typing import *
 
 import asyncio
+import time
 
 import click
 import uvloop
 
 from .midi import get_ports
+
+# types
+EventDelta = float  # in seconds
+TimeStamp = float  # time.time()
+MidiPacket = List[int]
+MidiMessage = Tuple[MidiPacket, EventDelta, TimeStamp]
 
 
 async def async_main() -> None:
@@ -16,6 +23,10 @@ async def async_main() -> None:
     except ValueError as port:
         click.secho(f"{port} is not available", fg="red", err=True)
         raise click.Abort
+
+    def midi_callback(msg: Tuple[MidiPacket, EventDelta], data: Any = None) -> None:
+        sent_time = time.time()
+        midi_packet, event_delta = msg
 
     from_circuit.set_callback(midi_callback)
     from_mono_station.close_port()
