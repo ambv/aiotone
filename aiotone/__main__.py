27f6cd689625f9340f91853d1b87e724209f6e26
Łukasz Langa <lukasz@langa.pl>
2020-04-18 13:54:32 +0200

    Queue, no loop or error handling

--- aiotone/__main__.py
+++ aiotone/__main__.py
@@ -17,6 +17,8 @@ MidiMessage = Tuple[MidiPacket, EventDelta, TimeStamp]
 
 
 async def async_main() -> None:
+    queue: asyncio.Queue[MidiMessage] = asyncio.Queue(maxsize=256)
+
     try:
         from_circuit, to_circuit = get_ports("Circuit", clock_source=True)
         from_mono_station, to_mono_station = get_ports("Circuit Mono Station")
@@ -27,9 +29,21 @@ async def async_main() -> None:
     def midi_callback(msg: Tuple[MidiPacket, EventDelta], data: Any = None) -> None:
         sent_time = time.time()
         midi_packet, event_delta = msg
+        midi_message = (midi_packet, event_delta, sent_time)
+        queue.put_nowait(midi_message)
 
     from_circuit.set_callback(midi_callback)
     from_mono_station.close_port()
+
+    await midi_consumer(queue)
+
+
+async def midi_consumer(queue: asyncio.Queue[MidiMessage]) -> None:
+    while True:
+        pkt, delta, sent_time = await queue.get()
+        latency = time.time() - sent_time
+        if __debug__:
+            click.echo(f"{pkt}\tevent delta: {delta:.4f}\tlatency: {latency:.4f}")
 
 
 @click.command()
