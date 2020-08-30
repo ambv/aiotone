3a332bf5287f5f1dd64a894b0a71dff9b156c82a
Łukasz Langa <lukasz@langa.pl>
2020-08-30 14:45:13 +0200

    Add support for Subharmonicon

--- aiotone/mothergen.py
+++ aiotone/mothergen.py
@@ -5,6 +5,7 @@ from typing import *
 
 import asyncio
 import configparser
+import functools
 import itertools
 from pathlib import Path
 import random
@@ -56,11 +57,16 @@ CONFIGPARSER_FALSE = {
 }
 REZ = {"rez", "resonance", "modwheel"}
 PORTAMENTO_MODES = REZ | CONFIGPARSER_FALSE
+CC = CONTROL_CHANGE
 
 
 class PlayAsyncFunction(Protocol):
     def __call__(
-        self, note: int, pulses: int, volume: int, decay: float = 0.5,
+        self,
+        note: int,
+        pulses: int,
+        volume: int,
+        decay: float = 0.5,
     ) -> Awaitable[None]:
         ...
 
@@ -70,17 +76,14 @@ class RawAsyncFunction(Protocol):
         ...
 
 
-class CcAsyncFunction(Protocol):
-    def __call__(self, type: int, value: int) -> Awaitable[None]:
-        ...
-
-
 @dataclass
 class Performance:
     red_port: MidiOut
     red_channel: int
     blue_port: MidiOut
     blue_channel: int
+    green_port: MidiOut
+    green_channel: int
     start_stop: bool
     portamento: str
     portamento_max: int
@@ -90,24 +93,40 @@ class Performance:
     last_expression_value: int = 64
     blue_sequencer: Optional[asyncio.Task] = None
     red_sequencer: Optional[asyncio.Task] = None
+    green_sequencer: Optional[asyncio.Task] = None
     key: List[int] = F
 
+    def __attrs_post_init__(self) -> None:
+        self.play_red = functools.partial(
+            self.play, out=self.red_port, channel=self.red_channel
+        )
+        self.play_blue = functools.partial(
+            self.play, out=self.blue_port, channel=self.blue_channel
+        )
+        self.play_green = functools.partial(
+            self.play, out=self.green_port, channel=self.green_channel
+        )
+
     async def setup(self) -> None:
+        # Like `__attrs_post_init__` but requires awaiting so a separate step.
         silence(self.red_port, stop=self.start_stop, channels=[self.red_channel])
         silence(self.blue_port, stop=self.start_stop, channels=[self.blue_channel])
-        await self.cc_red(MOD_WHEEL, 0)
-        await self.cc_blue(MOD_WHEEL, 0)
-        await self.cc_both(PORTAMENTO, 0)
-        await self.cc_both(PORTAMENTO_TIME, 0)
+        silence(self.green_port, stop=self.start_stop, channels=[self.green_channel])
+        await self.red(CC, MOD_WHEEL, 0)
+        await self.blue(CC, MOD_WHEEL, 0)
+        await self.green(CC, MOD_WHEEL, 0)
+        await self.all(CC, PORTAMENTO, 0)
+        await self.all(CC, PORTAMENTO_TIME, 0)
 
     async def play(
         self,
-        out: MidiOut,
-        channel: int,
         note: int,
         pulses: int,
         volume: int,
         decay: float = 0.5,
+        *,
+        out: MidiOut,
+        channel: int,
     ) -> None:
         note_on_length = int(round(pulses * decay, 0))
         rest_length = pulses - note_on_length
@@ -116,32 +135,27 @@ class Performance:
         out.send_message([NOTE_OFF | channel, note, volume])
         await self.wait(rest_length)
 
-    async def play_red(
-        self, note: int, pulses: int, volume: int, decay: float = 0.5,
-    ) -> None:
-        await self.play(self.red_port, self.red_channel, note, pulses, volume, decay)
-
-    async def play_blue(
-        self, note: int, pulses: int, volume: int, decay: float = 0.5,
-    ) -> None:
-        await self.play(self.blue_port, self.blue_channel, note, pulses, volume, decay)
-
     async def wait(self, pulses: int) -> None:
         await self.metronome.wait(pulses)
 
     def send_once(self, message: Sequence[int]) -> None:
-        """Ensure that each Mother receives this message only once.
-        
-        When both Mothers are on the same MIDI OUT port (just different channels),
+        """Ensure that each device receives this message only once.
+
+        When all devices are on the same MIDI OUT port (just different channels),
         certain MIDI messages which are channel agnostic, would be effectively sent
-        twice.  While that doesn't matter most of the time, when it does, use this
+        three times.  While that doesn't matter most of the time, when it does, use this
         method to ensure a message is only received once on each device.
         """
         rp = self.red_port
         bp = self.blue_port
+        gp = self.green_port
         rp.send_message(message)
         if bp is not rp:
             bp.send_message(message)
+        if gp is not rp:
+            gp.send_message(message)
+
+    # Messages received from `midi_consumer`
 
     async def clock(self) -> None:
         await self.metronome.tick()
@@ -153,11 +167,15 @@ class Performance:
         await self.metronome.reset()
         if not self.red_sequencer:
             self.red_sequencer = asyncio.create_task(
-                self.sequencer(self.play_red, self.cc_red, self.red)
+                self.mother_simple(self.play_red, self.red)
             )
         if not self.blue_sequencer:
             self.blue_sequencer = asyncio.create_task(
-                self.sequencer(self.play_blue, self.cc_blue, self.blue)
+                self.mother_simple(self.play_blue, self.blue)
+            )
+        if not self.green_sequencer:
+            self.green_sequencer = asyncio.create_task(
+                self.subharmonicon(self.play_green, self.green)
             )
 
     async def stop(self) -> None:
@@ -167,13 +185,65 @@ class Performance:
         if self.blue_sequencer:
             self.blue_sequencer.cancel()
             self.blue_sequencer = None
+        if self.green_sequencer:
+            self.green_sequencer.cancel()
+            self.green_sequencer = None
         if self.start_stop:
             self.send_once([STOP])
-        await self.cc_red(ALL_NOTES_OFF, 0)
-        await self.cc_blue(ALL_NOTES_OFF, 0)
+        await self.red(CC, ALL_NOTES_OFF, 0)
+        await self.blue(CC, ALL_NOTES_OFF, 0)
 
-    async def sequencer(
-        self, play: PlayAsyncFunction, cc: CcAsyncFunction, raw: RawAsyncFunction
+    async def note_on(self, note: int, volume: int) -> None:
+        for note_octaves in all_notes:
+            if note in note_octaves:
+                self.key = note_octaves
+                break
+
+    async def note_off(self, note: int) -> None:
+        ...
+
+    async def mod_wheel(self, value: int) -> None:
+        await self.red(CC, MOD_WHEEL, value)
+        if self.portamento not in REZ:
+            return
+
+        if value == 0:
+            await self.all(CC, PORTAMENTO, 0)
+            await self.all(CC, PORTAMENTO_TIME, 0)
+        else:
+            await self.all(CC, PORTAMENTO, 127)
+            converted_value = int(self.portamento_max * value / 127)
+            await self.all(CC, PORTAMENTO_TIME, converted_value)
+
+    async def expression(self, value: int) -> None:
+        self.last_expression_value = value
+        await self.blue(CC, MOD_WHEEL, value)
+
+    # Raw commands
+
+    async def red(self, event: int, note: int, volume: int) -> None:
+        self.red_port.send_message([event | self.red_channel, note, volume])
+
+    async def blue(self, event: int, note: int, volume: int) -> None:
+        self.blue_port.send_message([event | self.blue_channel, note, volume])
+
+    async def green(self, event: int, note: int, volume: int) -> None:
+        # print("->", event, note, volume)
+        self.green_port.send_message([event | self.green_channel, note, volume])
+
+    async def mothers(self, event: int, note: int, volume: int) -> None:
+        self.red_port.send_message([event | self.red_channel, note, volume])
+        self.blue_port.send_message([event | self.blue_channel, note, volume])
+
+    async def all(self, event: int, note: int, volume: int) -> None:
+        self.red_port.send_message([event | self.red_channel, note, volume])
+        self.blue_port.send_message([event | self.blue_channel, note, volume])
+        self.green_port.send_message([event | self.green_channel, note, volume])
+
+    # Sequencers
+
+    async def mother_simple(
+        self, play: PlayAsyncFunction, raw: RawAsyncFunction
     ) -> None:
         octaves = range(1, 7)
         speeds = (24, 24, 24, 24, 12, 12)
@@ -187,53 +257,67 @@ class Performance:
             interval = random.choice(intervals)
             await play(self.key[oct] + interval, speed, 64, decay)
 
-    async def note_on(self, note: int, volume: int) -> None:
-        for note_octaves in all_notes:
-            if note in note_octaves:
-                self.key = note_octaves
-                break
+    async def subharmonicon(
+        self, play: PlayAsyncFunction, raw: RawAsyncFunction
+    ) -> None:
+        """Subharmonicon-specific sequencing.
 
-    async def note_off(self, note: int) -> None:
-        ...
+        Works best with the following Subharmonicon settings:
+        - Quantize disabled;
+        - Polyrhythm section disabled (all four rhythms not having either SEQ1 nor
+          SEQ2 enabled);
+        - VCO1 and VCO2 tuned to the center position.
 
-    async def mod_wheel(self, value: int) -> None:
-        await self.cc_red(MOD_WHEEL, value)
-        if self.portamento not in REZ:
-            return
+        From the manual:
+        - VCO1 = CC4 0-127 + CC36 0-127
+        - VCO1 SUB1 = CC103 0-127 representing 16 divider values
+        - VCO1 SUB2 = CC104 0-127 representing 16 divider values
+        - VCO2 = CC12 0-127 + CC44 0-127
+        - VCO2 SUB1 = CC105 0-127 representing 16 divider values
+        - VCO2 SUB2 = CC106 0-127 representing 16 divider values
+        - VCF EG ATTACK = CC23 0-127 + CC55 0-127
+        - VCF EG DECAY = CC24 0-127 + CC56 0-127
+        - VCA EG ATTACK = CC28 0-127 + CC60 0-127
+        - VCA EG DECAY = CC29 0-127 + CC61 0-127
+        - Rhythm Generator Logic = CC113 0-63 OR (default) / 64-127 XOR
 
-        if value == 0:
-            await self.cc_both(PORTAMENTO, 0)
-            await self.cc_both(PORTAMENTO_TIME, 0)
-        else:
-            await self.cc_both(PORTAMENTO, 127)
-            converted_value = int(self.portamento_max * value / 127)
-            await self.cc_both(PORTAMENTO_TIME, converted_value)
+        16 divider values in subs are encoded like this:
+        - 0-7 = 16
+        - 8-15 = 15
+        - ...
+        - 120-127 = 1
+        """
 
-    async def expression(self, value: int) -> None:
-        self.last_expression_value = value
-        await self.cc_blue(MOD_WHEEL, value)
+        def shift(l: List[int]) -> int:
+            result = l.pop(1)
+            l.append(result)
+            return result
 
-    # Raw commands
+        # VCO 1
+        await raw(CC, 4, 64)
+        await raw(CC, 36, 64)
+        # VCO 2
+        await raw(CC, 12, 64)
+        await raw(CC, 44, 64)
+        # VCF attack
+        await raw(CC, 23, 0)
+        await raw(CC, 55, 0)
+        # VCF decay
+        await raw(CC, 24, 8)
+        await raw(CC, 56, 0)
+        # VCA attack
+        await raw(CC, 28, 8)
+        await raw(CC, 60, 0)
+        # VCA decay
+        await raw(CC, 29, 8)
+        await raw(CC, 61, 0)
 
-    async def red(self, event: int, note: int, volume: int) -> None:
-        self.red_port.send_message([event | self.red_channel, note, volume])
-
-    async def blue(self, event: int, note: int, volume: int) -> None:
-        self.blue_port.send_message([event | self.blue_channel, note, volume])
-
-    async def both(self, event: int, note: int, volume: int) -> None:
-        self.red_port.send_message([event | self.red_channel, note, volume])
-        self.blue_port.send_message([event | self.blue_channel, note, volume])
-
-    async def cc_red(self, type: int, value: int) -> None:
-        self.red_port.send_message([CONTROL_CHANGE | self.red_channel, type, value])
-
-    async def cc_blue(self, type: int, value: int) -> None:
-        self.blue_port.send_message([CONTROL_CHANGE | self.blue_channel, type, value])
-
-    async def cc_both(self, type: int, value: int) -> None:
-        self.red_port.send_message([CONTROL_CHANGE | self.red_channel, type, value])
-        self.blue_port.send_message([CONTROL_CHANGE | self.blue_channel, type, value])
+        div = [8 * i for i in range(16)] + [127]
+        while True:
+            await play(self.key[2], 12, 64, 1.0)
+            await self.wait(12)
+            await raw(CC, 103, shift(div))  # doesn't work?
+            await self.wait(12)
 
 
 @click.command()
@@ -329,6 +413,18 @@ async def async_main(config: str) -> None:
             click.secho(f"{port} not connected", fg="red", err=True)
             raise click.Abort
 
+    # Configure the `to_subharmonicon` port
+    if cfg["from-ableton"]["port-name"] == cfg["to-subharmonicon"]["port-name"]:
+        to_subharmonicon = to_ableton
+    elif cfg["to-mother-red"]["port-name"] == cfg["to-subharmonicon"]["port-name"]:
+        to_subharmonicon = to_mother_red
+    else:
+        try:
+            to_subharmonicon = get_out_port(cfg["to-subharmonicon"]["port-name"])
+        except ValueError as port:
+            click.secho(f"{port} not connected", fg="red", err=True)
+            raise click.Abort
+
     porta_mode = cfg["from-ableton"]["portamento"]
     if porta_mode not in PORTAMENTO_MODES:
         click.secho(
@@ -342,8 +438,10 @@ async def async_main(config: str) -> None:
     performance = Performance(
         red_port=to_mother_red,
         blue_port=to_mother_blue,
+        green_port=to_subharmonicon,
         red_channel=cfg["to-mother-red"].getint("channel") - 1,
         blue_channel=cfg["to-mother-blue"].getint("channel") - 1,
+        green_channel=cfg["to-subharmonicon"].getint("channel") - 1,
         start_stop=cfg["from-ableton"].getboolean("start-stop"),
         portamento=cfg["from-ableton"]["portamento"],
         portamento_max=cfg["from-ableton"].getint("portamento-max"),
@@ -403,14 +501,14 @@ async def midi_consumer(
             elif msg[1] == EXPRESSION_PEDAL:
                 await performance.expression(msg[2])
             elif msg[1] == SUSTAIN_PEDAL:
-                await performance.cc_both(SUSTAIN_PEDAL, msg[2])
+                await performance.all(CC, SUSTAIN_PEDAL, msg[2])
             elif msg[1] == ALL_NOTES_OFF:
-                await performance.cc_both(ALL_NOTES_OFF, msg[2])
+                await performance.all(CC, ALL_NOTES_OFF, msg[2])
             else:
                 print(f"warning: unhandled CC {msg}", file=sys.stderr)
         elif t == PITCH_BEND:
             # Note: this requires Mother 32 firmware 2.0.
-            await performance.both(PITCH_BEND, msg[1], msg[2])
+            await performance.all(PITCH_BEND, msg[1], msg[2])
         else:
             if st not in handled_types:
                 print(f"warning: unhandled event {msg}", file=sys.stderr)
