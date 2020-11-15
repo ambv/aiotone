fc3bcf59414a900a71f281979a8bfa15425b3e50
Łukasz Langa <lukasz@langa.pl>
2020-11-16 00:21:24 +0100

    Refactor sines and envelopes into resettable Operators

--- aiotone/fmsynth.py
+++ aiotone/fmsynth.py
@@ -5,6 +5,7 @@ from typing import *
 from array import array
 import asyncio
 import configparser
+from dataclasses import dataclass
 import math
 from pathlib import Path
 
@@ -59,27 +60,6 @@ def endless_sine(sample_count: int) -> Audio:
         want_frames = yield result[:want_frames]
 
 
-def envelope(mono: Audio, a: int, d: int) -> Audio:
-    """Envelope parameters are in samples."""
-    result = init(mono)
-    want_frames = yield result
-
-    out_buffer = array("h", [0] * want_frames)
-    samples_since_reset = 0
-    while True:
-        mono_buffer = mono.send(want_frames)
-        for i in range(want_frames):
-            if samples_since_reset <= a:
-                volume = samples_since_reset / a
-            elif samples_since_reset - a <= d:
-                volume = 1.0 - (samples_since_reset - a) / d
-            else:
-                volume = 0.0
-            out_buffer[i] = int(volume * mono_buffer[i])
-            samples_since_reset += 1
-        want_frames = yield out_buffer[:want_frames]
-
-
 def panning(mono: Audio, pan: float = 0.0) -> Audio:
     result = init(mono)
     want_frames = yield result
@@ -109,41 +89,104 @@ def auto_pan(mono: Audio, panner: Audio) -> Audio:
         want_frames = yield out_buffer[: 2 * want_frames]
 
 
-def stereo_mixer(synth: Synthesizer) -> Audio:
-    mix_down = 1 / synth.num_voices
-    stereo = [init(v) for v in synth.voices]
-    want_frames = yield stereo[0]
-
-    out_buffer = array("h", [0] * (2 * want_frames))
-    with profiling.maybe(DEBUG):
-        while True:
-            stereo = [v.send(want_frames) for v in synth.voices]
-            for i in range(0, 2 * want_frames):
-                out_buffer[i] = int(sum([mix_down * s[i] for s in stereo]))
-            want_frames = yield out_buffer[: 2 * want_frames]
-
-
 class Synthesizer:
     def __init__(self, *, polyphony: int) -> None:
-        endless_sines = self._gen_endless_sines()
-        self.panning = [(2 * i / (polyphony - 1) - 1) for i in range(polyphony)]
-        self.voices = [
-            panning(next(endless_sines), self.panning[i]) for i in range(polyphony)
-        ]
-        self.num_voices = len(self.voices)
+        self.polyphony = polyphony
+        self.reset_voices()
 
-    def _gen_endless_sines(self) -> Iterator[Audio]:
-        yield envelope(endless_sine(88 * 3), a=48, d=48000)
-        yield envelope(endless_sine(66 * 3), a=480, d=48000)
-        yield envelope(endless_sine(99), a=4800, d=48000)
-        yield envelope(endless_sine(44), a=96000, d=48000)
-        yield envelope(endless_sine(88 * 4), a=48000, d=48000)
+    def reset_voices(self) -> None:
+        polyphony = self.polyphony
+        operators = self._gen_operators()
+        self.panning = [(2 * i / (polyphony - 1) - 1) for i in range(polyphony)]
+        self.voices = [next(operators) for i in range(polyphony)]
+
+    def stereo_out(self) -> Audio:
+        """A stereo mixer."""
+
+        voices = [
+            panning(self.voices[i].mono_out(), self.panning[i])
+            for i in range(self.polyphony)
+        ]
+
+        mix_down = 1 / self.polyphony
+        stereo = [init(v) for v in voices]
+        want_frames = yield stereo[0]
+
+        out_buffer = array("h", [0] * (2 * want_frames))
+        with profiling.maybe(DEBUG):
+            while True:
+                stereo = [v.send(want_frames) for v in voices]
+                for i in range(0, 2 * want_frames):
+                    out_buffer[i] = int(sum([mix_down * s[i] for s in stereo]))
+                want_frames = yield out_buffer[: 2 * want_frames]
+
+    def _gen_operators(self) -> Iterator[Operator]:
+        while True:
+            yield Operator(endless_sine(88 * 3), a=48, d=48000)
+            yield Operator(endless_sine(66 * 3), a=480, d=48000)
+            yield Operator(endless_sine(99), a=4800, d=48000)
+            yield Operator(endless_sine(44), a=96000, d=48000)
+            yield Operator(endless_sine(88 * 4), a=48000, d=48000)
+
+
+@dataclass
+class Operator:
+    wave: Audio
+    a: int  # in number of samples
+    d: int  # in number of samples
+    volume: float = 0.0  # 0.0 - 1.0; current volume, not used for relative attenuation
+    samples_since_reset: int = -1
+    reset: bool = False
+
+    def mono_out(self) -> Audio:
+        """A resettable envelope."""
+        result = init(self.wave)
+        want_frames = yield result
+
+        out_buffer = array("h", [0] * want_frames)
+        while True:
+            wave = self.wave
+            volume = self.volume
+            a = self.a
+            d = self.d
+            samples_since_reset = self.samples_since_reset
+            if samples_since_reset == -1:
+                for i in range(want_frames):
+                    out_buffer[i] = 0
+            else:
+                mono_buffer = wave.send(want_frames)
+                for i in range(want_frames):
+                    if samples_since_reset <= a:
+                        volume = samples_since_reset / a
+                    elif samples_since_reset - a <= d:
+                        volume = 1.0 - (samples_since_reset - a) / d
+                    else:
+                        volume = 0.0
+                    out_buffer[i] = int(volume * mono_buffer[i])
+                    samples_since_reset += 1
+            if self.reset:
+                self.reset = False
+                self.samples_since_reset = 0
+                self.volume = 0.0
+            elif volume > 0:
+                self.samples_since_reset = samples_since_reset
+                self.volume = volume
+            else:
+                self.samples_since_reset = -1
+                self.volume = 0.0
+            want_frames = yield out_buffer[:want_frames]
 
 
 async def async_main(synth: Synthesizer) -> None:
     # TODO: respond to MIDI like in `redblue` and `mothergen`.
+    i = 0
     while True:
-        await asyncio.sleep(1)
+        if i % (synth.polyphony / 2) == 0:
+            await asyncio.sleep(2.5)
+        else:
+            await asyncio.sleep(0.1)
+        synth.voices[i % synth.polyphony].reset = True
+        i += 1
 
 
 @click.command()
@@ -187,8 +230,8 @@ def main(config: str, make_config: bool) -> None:
         output_format=miniaudio.SampleFormat.SIGNED16,
         buffersize_msec=buffer_msec,
     ) as dev:
-        synth = Synthesizer(polyphony=5)
-        stream = stereo_mixer(synth)
+        synth = Synthesizer(polyphony=10)
+        stream = synth.stereo_out()
         init(stream)
         dev.start(stream)
         try:
