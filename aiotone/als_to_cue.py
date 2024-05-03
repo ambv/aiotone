aae8ccc17404d2b5557b8e2d075a0f6c378dc29b
Łukasz Langa <lukasz@langa.pl>
2024-05-03 17:41:48 +0200

    Support Live 12 files, add display of concise timestamps

--- aiotone/als_to_cue.py
+++ aiotone/als_to_cue.py
@@ -7,9 +7,11 @@ NOTE: this will report wrong times if the project's tempo is variable.
 """
 
 import csv
+from dataclasses import dataclass
 import gzip
 from pathlib import Path
 import re
+import sys
 import xml.etree.ElementTree as ET
 
 import click
@@ -37,10 +39,14 @@ def convert_als_to_csv(input: Path) -> Path:
     tree = ET.parse(f)
     root = tree.getroot()
 
+    path = "DeviceChain/Mixer/Tempo/Manual"
     tempo: float = 120.0
-    tempo_el = root.find("*/MasterTrack/DeviceChain/Mixer/Tempo/Manual")
-    if tempo_el is not None:
+    if (tempo_el := root.find(f"LiveSet/MasterTrack/{path}")) is not None:
         tempo = float(tempo_el.get("Value") or 120.0)
+    elif (tempo_el := root.find(f"LiveSet/MainTrack/{path}")) is not None:
+        tempo = float(tempo_el.get("Value") or 120.0)
+    else:
+        print("warning: tempo assumed as 120 BPM", file=sys.stderr)
 
     divider = tempo / 60
 
@@ -60,33 +66,79 @@ def convert_als_to_csv(input: Path) -> Path:
             t = float(time_el.get("Value") or 0) / divider
             time = f"{t // 3600:01.0f}:{(t % 3600) // 60:02.0f}:{int(t % 60):02d}.{1000 * (t - int(t)):03.0f}"
             name = name_el.get("Value")
-            writer.writerow(["", name, time])
+            writer.writerow(["", name, time, t])
 
     return output
 
 
+@dataclass
+class Chapter:
+    title: str
+    timestamp: float
+    ts_text_minutes: str
+    ts_text_hours: str
+
+
 @click.command()
-# @click.option("--quiet", is_flag=True, default=False)
+@click.option(
+    "-c/-C",
+    "--show-cue/--hide-cue",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Display the .cue sheet file",
+)
+@click.option(
+    "-t/-T",
+    "--show-ts/--hide-ts",
+    is_flag=True,
+    show_default=True,
+    default=True,
+    help="Display concise timestamps",
+)
 @click.argument("input")
-def main(input: str) -> None:
+def main(input: str, show_cue: bool, show_ts: bool) -> None:
     input_path = Path(input)
     if input_path.suffix == ".als":
         input_path = convert_als_to_csv(input_path)
     output_path = input_path.with_suffix(".cue")
-    track = 0
-    with open(input_path) as f_in, open(output_path, "w") as f_out:
-        c = csv.reader(f_in)
+    chapters = []
+    with open(input_path) as f_in:
+        csv_file = csv.reader(f_in)
+        for row in csv_file:
+            try:
+                ts_raw = float(row[3])
+                ts_hours = row[2]
+                ts_mins = convert_timestamp(ts_hours)
+            except (IndexError, ValueError):
+                print("invalid row:", row, file=sys.stderr)
+                raise
+            c = Chapter(
+                title=row[1],
+                timestamp=ts_raw,
+                ts_text_minutes=ts_mins,
+                ts_text_hours=ts_hours.split(".")[0],
+            )
+            chapters.append(c)
+
+    chapters.sort(key=lambda c: c.timestamp)
+
+    with open(output_path, "w") as f_out:
         f_out.write('TITLE ""\n')
         f_out.write('FILE "" WAVE\n')
-        for row in c:
-            track += 1
-            timestamp = convert_timestamp(row[2])
-            f_out.write(f"  TRACK {track:0>2} AUDIO\n")
-            f_out.write(f'    TITLE "{row[1]}"\n')
-            f_out.write(f"    INDEX 01 {timestamp}\n")
-    with open(output_path) as f:
-        for i, line in enumerate(f.readlines()):
-            print(i + 1, line.rstrip())
+        for i, c in enumerate(chapters, 1):
+            f_out.write(f"  TRACK {i:0>2} AUDIO\n")
+            f_out.write(f'    TITLE "{c.title}"\n')
+            f_out.write(f"    INDEX 01 {c.ts_text_minutes}\n")
+
+    if show_cue:
+        with open(output_path) as f:
+            for i, line in enumerate(f.readlines()):
+                print(i + 1, line.rstrip())
+
+    if show_ts:
+        for c in chapters:
+            print(f"[{c.ts_text_hours:>08}] {c.title}")
 
 
 if __name__ == "__main__":
