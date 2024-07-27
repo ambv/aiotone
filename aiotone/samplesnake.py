2e71765351e8b8f256e792b11c8dda08545ff9fc
Łukasz Langa <lukasz@langa.pl>
2024-07-27 23:10:45 +0200

    Add support for recording to wav files

--- aiotone/samplesnake.py
+++ aiotone/samplesnake.py
@@ -2,9 +2,9 @@
 
 NEXT STEPS:
 - [x] play a note via MIDI with some set velocity
-- [ ] record the sample until silence
+- [x] record the sample until silence
 - [ ] left and right silence trim
-- [ ] save a WAV file
+- [x] save a WAV file
 """
 
 from __future__ import annotations
@@ -40,6 +40,8 @@ CONFIGPARSER_FALSE = {
     if v is False
 }
 SILENCE = Event()
+CAN_RECORD = Event()
+RECORDED_FILE_NAME: str = ""
 
 
 def get_device(devices: list[dict[str, object]], name: str) -> str:
@@ -57,6 +59,47 @@ def get_buffer_format() -> Literal["i", "l", "f"]:
             return letter  # type: ignore
 
     raise LookupError("This is an unsupported machine.")
+
+
+def save_recorded_audio(
+    buffer: array[float], sample_rate: int, channel_count: int, sample_directory: Path
+) -> None:
+    if not RECORDED_FILE_NAME:
+        return
+
+    fn = (sample_directory / RECORDED_FILE_NAME).with_suffix(".wav")
+    sound_file = miniaudio.DecodedSoundFile(
+        RECORDED_FILE_NAME,
+        channel_count,
+        sample_rate,
+        miniaudio.SampleFormat.FLOAT32,
+        buffer,
+    )
+    sound_file.sub_format = miniaudio.lib.DR_WAVE_FORMAT_IEEE_FLOAT
+    miniaudio.wav_write_file(str(fn), sound_file)
+
+
+def record_audio_py(
+    in_buffer: array[float],
+    in_l: int,
+    in_r: int,
+    in_channel_count: int,
+    out_buffer: array[float],
+    out_offset: int,
+) -> None:
+    in_offset = 0
+    while (
+        in_offset - in_l < len(in_buffer)
+        and in_offset - in_r < len(in_buffer)
+        and out_offset - 1 < len(out_buffer)
+    ):
+        out_buffer[out_offset] = in_buffer[in_offset + in_l]
+        out_buffer[out_offset + 1] = in_buffer[in_offset + in_r]
+        in_offset += in_channel_count
+        out_offset += 2
+
+
+record_audio = record_audio_py
 
 
 def move_audio_py(
@@ -82,10 +125,13 @@ def move_audio_py(
 
 
 def process_audio(
+    sample_rate: int,
     channel_count: int,
     record_channels: list[int],
     play_channels: list[int],
     silence_threshold: float,
+    sample_max_length: int,  # in samples
+    sample_directory: Path,
 ) -> Generator[memoryview | array[float] | bytes, bytes, None]:
     nan = float("nan")
     buffer_format = get_buffer_format()
@@ -95,27 +141,54 @@ def process_audio(
     # NOTE: `input_bytes` is always a bytearray;
     # we need to convert to array.array manually.
     in_buffer: array[float] = array(buffer_format, input_bytes)
+    in_sample_count = len(in_buffer) // channel_count
     chan_sum = array(buffer_format, [0.0] * channel_count)
+    chan_sum_zero_bytes = bytearray(chan_sum.tobytes())
+    record_buffer_zero_bytes = bytearray(b"\x00" * 4 * sample_max_length * 2)
+    record_buffer: array[float] = array(buffer_format, record_buffer_zero_bytes)
     in_l, in_r = record_channels
     out_l, out_r = play_channels
     silent_iterations = 0
+    record_offset = -1  # -1 == we're not recording
     while True:
-        in_buffer_state = ""
-        saw_nan = False
         move_audio(in_buffer, in_l, in_r, out_buffer, out_l, out_r, chan_sum)
-        for ch in range(channel_count):
-            if chan_sum[ch] is nan:
-                saw_nan = True
-            in_buffer_state += f"{chan_sum[ch]:+09.5f} "
+
         if chan_sum[in_l] < silence_threshold and chan_sum[in_r] < silence_threshold:
             if not SILENCE.is_set():
                 silent_iterations += 1
                 if silent_iterations >= 50:
-                    SILENCE.set()
+                    click.secho(f"\nSaving {RECORDED_FILE_NAME}")
+                    save_recorded_audio(
+                        record_buffer[:record_offset], sample_rate, 2, sample_directory
+                    )
+                    update_buffer(record_buffer, record_buffer_zero_bytes)
                     silent_iterations = 0
+                    record_offset = -1
+                    SILENCE.set()
+                    CAN_RECORD.clear()
         else:
             SILENCE.clear()
+            if record_offset == -1 and CAN_RECORD.is_set():
+                click.secho(f"\nRecording {RECORDED_FILE_NAME}")
+                record_offset = 0
 
+        if record_offset >= 0:
+            record_audio(
+                in_buffer,
+                in_l,
+                in_r,
+                channel_count,
+                record_buffer,
+                record_offset,
+            )
+            record_offset += 2 * in_sample_count
+
+        in_buffer_state = ""
+        saw_nan = False
+        for ch in range(channel_count):
+            if chan_sum[ch] is nan:
+                saw_nan = True
+            in_buffer_state += f"{chan_sum[ch]:+09.5f} "
         print(
             f"{in_buffer.typecode}{out_buffer.typecode}"
             # f" {in_buffer.buffer_info()}"
@@ -128,26 +201,33 @@ def process_audio(
         )
         if saw_nan:
             print("!")
+
         input_bytes = yield out_mem
         update_buffer(in_buffer, input_bytes)
-        for ch in range(channel_count):
-            chan_sum[ch] = 0.0
+        update_buffer(chan_sum, chan_sum_zero_bytes)
 
 
 def play_notes(
     midi_out: midi.MidiOut, channel: int, sampling: configparser.SectionProxy
 ) -> None:
+    global RECORDED_FILE_NAME
+
     hold = sampling.getseconds("hold")
     cooldown = sampling.getseconds("cooldown")
     midi.silence(midi_out, channels=[channel])
     for note in sampling.getnotes("notes"):
         for octave in sorted(sampling.getintlist("octaves")):
             for velocity in sorted(sampling.getintlist("velocities")):
+                n = note[octave]
+                n_str = notes.note_to_name[n]
                 click.secho("\nWaiting for silence...", fg="blue")
                 SILENCE.wait()
                 time.sleep(cooldown)
-                n = note[octave]
-                n_str = notes.note_to_name[n]
+                RECORDED_FILE_NAME = f"{n_str}-{velocity}"
+                if prefix := sampling.get("sample-name-prefix"):
+                    RECORDED_FILE_NAME = f"{prefix}-{RECORDED_FILE_NAME}"
+                CAN_RECORD.set()
+                time.sleep(0.002)
                 click.secho(f"\n{n_str} NOTE ON at {velocity}...", fg="green")
                 midi_out.send_message([midi.NOTE_ON | channel, n, velocity])
                 time.sleep(hold)
@@ -246,18 +326,24 @@ def main(config: str, make_config: bool) -> None:
     playback_name = audio_out["name"]
     playback_channel_count = audio_out.getint("channel-count")
     playback_channels = audio_out.getchannels("play")
+    sample_rate = audio_in.getint("sample-rate")
     capture_name = audio_in["name"]
     capture_channel_count = audio_in.getint("channel-count")
     capture_channels = audio_in.getchannels("record")
     sampling = cfg["sampling"]
     silence_threshold = sampling.getfloat("silence-threshold")
+    sample_max_length = int(sampling.getseconds("sample-max-length") * sample_rate)
+
+    sample_directory = Path(sampling["sample-directory"])
+    if not sample_directory.is_dir():
+        sample_directory.mkdir()
 
     audio_devices = miniaudio.Devices()
     playback_id = get_device(audio_devices.get_playbacks(), playback_name)
     capture_id = get_device(audio_devices.get_captures(), capture_name)
 
     audio_device = miniaudio.DuplexStream(
-        sample_rate=audio_in.getint("sample-rate"),
+        sample_rate=sample_rate,
         buffersize_msec=2,
         playback_device_id=playback_id,
         playback_format=miniaudio.SampleFormat.FLOAT32,
@@ -265,6 +351,9 @@ def main(config: str, make_config: bool) -> None:
         capture_device_id=capture_id,
         capture_format=miniaudio.SampleFormat.FLOAT32,
         capture_channels=capture_channel_count,
+    )
+    print(
+        "audio device format", audio_device.playback_format, audio_device.capture_format
     )
 
     try:
@@ -281,10 +370,13 @@ def main(config: str, make_config: bool) -> None:
 
     with audio_device as audio:
         stream = process_audio(
+            sample_rate,
             capture_channel_count,
             capture_channels,
             playback_channels,
             silence_threshold,
+            sample_max_length,
+            sample_directory,
         )
         next(stream)
         gc.freeze()  # decrease the pool of garbage-collected memory
